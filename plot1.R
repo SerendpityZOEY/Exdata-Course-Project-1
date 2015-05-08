16f3cb382a61708f90ad9e04aae9323d3dba2aa9
SerendpityZOEY <zoey.zhang513@hotmail.com>
2015-05-08 17:18:02 +0800

    Update and rename LICENSE.md to plot1.R

---
+++ plot1.R
@@ -0,0 +1,17 @@
+## Read the raw data
+
+mydata <- read.table("household_power_consumption.txt", header=T, sep=';', 
+                     na.strings="?",check.names=F, stringsAsFactors=F, 
+                     comment.char="", quote='\"')
+                     
+## Subset the data
+
+data<-sqldf("select * from mydata where Date='1/2/2007' or Date='2/2/2007'")
+
+## Plot1
+hist(data$Global_active_power,col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+
+dev.copy(png,file="plog1.png")
+dev.off()
