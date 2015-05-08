97b850b384801b5ba0ad65c6053e707fb7aae02a
SerendpityZOEY <zoey.zhang513@hotmail.com>
2015-05-08 17:21:14 +0800

    Update plot1.R
    set size of png

--- plot1.R
+++ plot1.R
@@ -13,5 +13,5 @@ hist(data$Global_active_power,col="red",
      main="Global Active Power",
      xlab="Global Active Power (kilowatts)")
 
-dev.copy(png,file="plog1.png")
+dev.copy(png,file="plog1.png",width=480,height=480)
 dev.off()
